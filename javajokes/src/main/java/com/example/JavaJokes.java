61a74bd597e58c518ddc6948c1dac0491fc911e1
Rahul Grover <rahul.grover69@gmail.com>
2017-11-04 18:35:38 +0530

    Signed-off-by: Rahul Grover <rahul.grover69@gmail.com>

---
+++ javajokes/src/main/java/com/example/JavaJokes.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class JavaJokes {
+    public static String Joker (){
+
+
+        return "hi this is joker submission";
+    }
+
+    public String Jokes(){
+
+
+        return "My Own Joke";
+    }
+}
